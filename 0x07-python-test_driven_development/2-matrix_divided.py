487d1f8476b73ab8d880b6d20b1d960328a305a0
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2024-01-08 09:14:05 -0800

    python tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-"""
-This is the matrix_divided module.
-
-This module supplies one function, matrix_divided().
-"""
+"""defines function to scalar divde matrix"""
 
 
 def matrix_divided(matrix, div):
-    """
-    Return a new matrix where each element has been divided by div.
-
-    Args:
-        matrix (list): list of lists of integers or floats.
-        div (int, float): the divider, >= 0.
-    """
-    mtrx_type_e = 'matrix must be a matrix (list of lists) of integers/floats'
-    mtrx_len_e = 'Each row of the matrix must have the same size'
-    div_type_e = 'div must be a number'
-    div_zero_e = 'division by zero'
-
-    row_len = 0
+    """divides matrix by scalar integer, rounded to two decimal places"""
+    import decimal
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list:
-        raise TypeError(mtrx_type_e)
-
+        raise TypeError(error_msg)
+    len_rows = []
+    row_count = 0
     for row in matrix:
         if type(row) is not list:
-            raise TypeError(mtrx_type_e)
-        for i in row:
-            if type(i) not in [int, float]:
-                raise TypeError(mtrx_type_e)
-        if len(row) is not row_len and row_len is not 0:
-            raise TypeError(mtrx_len_e)
-        row_len = len(row)
-
+            raise TypeError(error_msg)
+        len_rows.append(len(row))
+        for element in row:
+            if type(element) not in [int, float]:
+                raise TypeError(error_msg)
+        row_count += 1
+    if len(set(len_rows)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
     if type(div) not in [int, float]:
-        raise TypeError(div_type_e)
-    if div is 0:
-        raise ZeroDivisionError(div_zero_e)
-
-    return [[round(nb / div, 2) for nb in row] for row in matrix]
+        raise TypeError("div must be a number")
+    if int(div) == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = list(map(lambda row:
+                          list(map(lambda x: round(x/div, 2), row)), matrix))
+    return new_matrix
